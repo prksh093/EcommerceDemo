11e5246a9be0b20ec122143d774064bf4f306491
prksh093 <prksh093@gmail.com>
2025-03-20 20:55:42 +0530

    LoginAssert Add

--- tests/src/test/java/testcases/loginwithASSERT.java
+++ tests/src/test/java/testcases/loginwithASSERT.java
@@ -1,12 +1,14 @@
-import java.time.Duration;
+package testcases;
 
+
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class Login {
+public class loginwithASSERT {
 
     static WebDriver driver;
 
@@ -15,33 +17,34 @@ public class Login {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
         driver.get("https://automationteststore.com/");
+        assert driver.getCurrentUrl().contains("automationteststore") : "Failed to navigate to the website";
 
-        //WebElement loginRegister = driver.findElement(By.partialLinkText("Login or register"));
-        //loginRegister.click();
+        WebElement lnkAccount = driver.findElement(By.xpath("(//span[text()='Account'])[1]"));
+        assert lnkAccount != null : "Account link not found";
 
-        WebElement lnkAccount= driver.findElement(By.xpath("(//span[text()='Account'])[1]"));
-        Actions ac= new Actions(driver);
+        Actions ac = new Actions(driver);
         ac.moveToElement(lnkAccount).click().perform();
-
-    
-
-        
+        assert lnkAccount.isDisplayed() : "Account link is not visible after action";
 
         WebElement UserName = driver.findElement(By.id("loginFrm_loginname"));
+        assert UserName != null : "Username field not found";
         UserName.sendKeys("prkshraj");
 
         WebElement Pwd = driver.findElement(By.id("loginFrm_password"));
+        assert Pwd != null : "Password field not found";
         Pwd.sendKeys("Admin@123");
 
         WebElement btnlogin = driver.findElement(By.xpath("//button[@title='Login']"));
+        assert btnlogin != null : "Login button not found";
         btnlogin.submit();
 
-    assert.assert(btnlogin);
-
-
+        assert driver.getTitle().contains("My Account") : "Login may have failed - title does not match expected";
 
         driver.quit();
     }
-
 }
+
+
+
